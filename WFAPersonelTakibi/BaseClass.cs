a6a9a9aac93ab880852eb8a1b58e53785e0de349
Sümeyye KAYA <sumeyye.kaya@baogrenci.com>
2019-11-19 14:12:13 +0300

    Add project files.

---
+++ WFAPersonelTakibi/BaseClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFAPersonelTakibi
+{
+    public abstract class BaseClass
+    {
+        public BaseClass()
+        {
+            this.CreatedDate = DateTime.Now;
+            this.CreatedComputerName = Environment.MachineName;
+            this.CreatedIP = "127.0.0.1";
+        }
+
+        public DateTime CreatedDate { get; set; }
+        public string CreatedIP { get; set; }
+        public string CreatedComputerName { get; set; }
+        public int ID { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string ModifiedIP { get; set; }
+        public string ModifiedComputerName { get; set; }
+        
+    }
+
+    public sealed class Personel : BaseClass
+    {
+        public string Adi { get; set; }
+        public string SoyAdi { get; set; }
+        public Gender Cinsiyet { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public string Mail { get; set; }
+        public string Telefon { get; set; }
+        public Department Departman { get; set; }
+        public string Adres { get; set; }
+        public string ImgURL  { get; set; }
+    }
+    public enum Gender { Male, Female, Karışık }
+    public enum Department { Finans, İK, Hukuk, Pazarlama, Satış, ARGE, Operasyon, Yönetim, GenelSekreterlik }
+}
---
+++ WFAPersonelTakibi/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace WFAPersonelTakibi
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbDepartment.Items.AddRange(Enum.GetNames(typeof(Department)));
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            Personel p = new Personel();
+            p.Adi = txtFirstName.Text;
+            p.SoyAdi = txtLastName.Text;
+            p.Adres = txtAddress.Text;
+            p.Departman = (Department)Enum.Parse(typeof(Department), cmbDepartment.Text);
+            p.DogumTarihi = dtBirthDate.Value;
+            p.Mail = txtMail.Text;
+            p.Telefon = txtPhone.Text;
+
+            if (rdFemale.Checked)
+            {
+                p.Cinsiyet = Gender.Female;
+            }
+            else if (rdMale.Checked)
+            {
+                p.Cinsiyet = Gender.Male;
+            }
+            else if (rdRandom.Checked)
+            {
+                p.Cinsiyet = Gender.Karışık;
+            }
+        }
+    }
+}
